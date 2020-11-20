8efa21f6cdcb136108f0689c9e93a6057b6dbc37
markus1203 <60688751+markus1203@users.noreply.github.com>
2020-11-20 15:36:00 +0100

    Update Sensor_cron.py

--- Sensor_cron.py
+++ Sensor_cron.py
@@ -123,7 +123,8 @@ try:
     if v < 0.1: v_tmrt= 0.1
     else: v_tmrt= v
     try:
-        tmrt=((((bg_calib+273.15)**4)+((1.1*(10**8)*(v_tmrt**0.6))/(0.95*(0.04**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
+        tmrt=((((bg_calib+273.15)**4)+((4.15*(10**8)*(v_tmrt**0.148))/(0.95*(0.04**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
+      #  tmrt=((((bg_calib+273.15)**4)+((1.1*(10**8)*(v_tmrt**0.6))/(0.95*(0.04**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
     except (ValueError):
         tmrt=-9999
     #-9999 #needed after Thorsson et al 2007: Tg = the globe temperature (°C),Va = the air velocity (ms−1),Ta = the air temperature (°C),D = the globe diameter (mm),ε = the globe emissivity
