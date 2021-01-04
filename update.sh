fd6332bafb9cbe8ea24efe53cd6591ba3754504e
markus1203 <60688751+markus1203@users.noreply.github.com>
2021-01-04 11:41:18 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ cd /home/pi
 git clone https://Markus1203:MeteoMarkus3@github.com/markus1203/MoBiMet_update.git
 sudo cp -r /home/pi/MoBiMet_update/*.py /home/pi/Desktop/py-Scripts/
 sudo cp -r /home/pi/MoBiMet_update/*.csv /home/pi/Desktop/
+sudo cp -r /home/pi/MoBiMet_update/*.sh /home/pi/
 sudo rm -r /home/pi/MoBiMet_update/
