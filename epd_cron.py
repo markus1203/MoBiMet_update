8b0efd3a82dc65b6714d769b817cd316ed70cb9c
markus1203 <60688751+markus1203@users.noreply.github.com>
2021-02-10 10:11:33 +0100

    Update epd_cron.py

--- epd_cron.py
+++ epd_cron.py
@@ -114,29 +114,29 @@ if lang=='g':
         drawblack.text((130,120), ' keine', font = font24, fill = 0)
         drawblack.text((130,145), ' Belastung', font = font24, fill = 0)
     elif float(PET) <= 4 and float(PET)<-9999:
-        drawblack.text((130,120), ' extremer', font = font24, fill = 0)
-        drawblack.text((130,145), u' Kältestress', font = font24, fill = 0)
+        drawblack.text((130,120), ' extreme', font = font24, fill = 0)
+        drawblack.text((130,145), u' Kältebelastung', font = font24, fill = 0)
     elif float(PET) <= 8:
-        drawblack.text((130,120), ' starker', font = font24, fill = 0)
-        drawblack.text((130,145), u' Kältestress', font = font24, fill = 0)
+        drawblack.text((130,120), ' starke', font = font24, fill = 0)
+        drawblack.text((130,145), u' Kältebelastung', font = font24, fill = 0)
     elif float(PET) <= 13:
-        drawblack.text((130,120), ' moderater', font = font24, fill = 0)
-        drawblack.text((130,145), u' Kältestress', font = font24, fill = 0)
+        drawblack.text((130,120), ' moderate', font = font24, fill = 0)
+        drawblack.text((130,145), u' Kältebelastung', font = font24, fill = 0)
     elif float(PET) <= 18:
-        drawblack.text((130,120), ' leichter', font = font24, fill = 0)
-        drawblack.text((130,145), u' Kältestress', font = font24, fill = 0)
+        drawblack.text((130,120), ' leichte', font = font24, fill = 0)
+        drawblack.text((130,145), u' Kältebelastung', font = font24, fill = 0)
     elif float(PET)>=41:
-        drawblack.text((130,120), ' extremer', font = font24, fill = 0)
-        drawblack.text((130,145), u' Hitzestress', font = font24, fill = 0)
+        drawblack.text((130,120), ' extreme', font = font24, fill = 0)
+        drawblack.text((130,145), u' Hitzebelastung', font = font24, fill = 0)
     elif float(PET)>=35:
         drawblack.text((130,120), ' starker', font = font24, fill = 0)
-        drawblack.text((130,145), u' Hitzestress', font = font24, fill = 0)
+        drawblack.text((130,145), u' Hitzebelastung', font = font24, fill = 0)
     elif float(PET)>=29:
         drawblack.text((130,120), ' moderater', font = font24, fill = 0)
-        drawblack.text((130,145), u' Hitzestress', font = font24, fill = 0)
+        drawblack.text((130,145), u' Hitzebelastung', font = font24, fill = 0)
     elif float(PET)>=23:
         drawblack.text((130,120), ' leichter', font = font24, fill = 0)
-        drawblack.text((130,145), u' Hitzestress', font = font24, fill = 0)
+        drawblack.text((130,145), u' Hitzebelastung', font = font24, fill = 0)
         
 if lang=='f':
     drawred.text((0,30), u" Température",font = font16, fill = 0)
