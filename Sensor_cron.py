ff5c09b7b61310663c4038c6afcc453d34578452
markus1203 <60688751+markus1203@users.noreply.github.com>
2021-02-18 18:04:24 +0100

    Update Sensor_cron.py

--- Sensor_cron.py
+++ Sensor_cron.py
@@ -200,7 +200,7 @@ else:
     #print("Nodes temperature [T_core, T_skin, T_clo]",Tstable)
     #print('Thermal Balance', Syst(Tstable, Ta, Tmrt, HR, v, age, sex, ht, mbody, pos, M, icl,True)[0])
     #print('PET:', round(PET(age, sex, ht, mbody, pos, M, icl, Tstable, Tmin, Tmax, eps),2))
-    pet = round(PET(age, sex, ht, mbody, pos, M, icl, Tstable, Tmin, Tmax, eps),2)
+    pet = round(PET(age, sex, ht, mbody, pos, M, icl, Tstable, Tmin, Tmax, eps),1)
     sl_pet=stresslevel_PET(pet)
     comf_pet=comfortable_PET(pet)
 
