5e0ce3f846f7c5ae23a5177f4a5ffcbf8e6afb8a
markus1203 <60688751+markus1203@users.noreply.github.com>
2021-07-06 11:40:00 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,3 @@ sudo cp -r /home/pi/MoBiMet_update/*.py /home/pi/Desktop/py-Scripts/
 sudo cp -r /home/pi/MoBiMet_update/*.csv /home/pi/Desktop/
 sudo cp -r /home/pi/MoBiMet_update/*.sh /home/pi/
 sudo rm -r /home/pi/MoBiMet_update/
-sudo reboot
