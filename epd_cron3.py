26a060787ddec2152cef2c69d16d9e9ebbb73968
markus1203 <60688751+markus1203@users.noreply.github.com>
2022-03-21 11:50:30 +0100

    Update epd_cron3.py

--- epd_cron3.py
+++ epd_cron3.py
@@ -63,7 +63,11 @@ f1.close()
 
 IP=(last_line.split(',')[2])
 time=(last_line.split(',')[0])
-dht22_humidity=(last_line.split(',')[5])
+#dht22_humidity=(last_line.split(',')[5])
+if (last_line.split(',')[25])  is not None:
+    dht22_humidity=str(float(last_line.split(',')[25]))
+else:
+    dht22_humidity=(last_line.split(',')[5])
 dht22_temperature=(last_line.split(',')[7])
 PET=(last_line.split(',')[19])
 #comf=comfortable_PET(float(PET))
