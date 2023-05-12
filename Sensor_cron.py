6989aaa4dcad0c52d1becd4fcdc0381b68e54307
markus1203 <60688751+markus1203@users.noreply.github.com>
2023-05-12 13:43:10 +0200

    Update Sensor_cron.py
    if bg_raw is 85 than -9999

--- Sensor_cron.py
+++ Sensor_cron.py
@@ -134,14 +134,19 @@ try:
           return temp_c
 
     bg_raw = grad_lesen()
-    bg_calib = bg_cal_a0+ bg_raw * bg_cal_a1
-    if v < 0.1: v_tmrt= 0.1
-    else: v_tmrt= v
-    try:
-        tmrt=((((bg_calib+273.15)**4)+((1.54023939374378*(10**8)*(v_tmrt**0.462186526822537))/(0.95*(0.05**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
-      #  tmrt=((((bg_calib+273.15)**4)+((1.1*(10**8)*(v_tmrt**0.6))/(0.95*(0.04**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
-    except (ValueError):
-        tmrt=-9999
+    if bg_raw==85:
+        bg_raw=-9999
+        bg_calib = -9999
+        tmrt= -9999
+    else:
+        bg_calib = bg_cal_a0+ bg_raw * bg_cal_a1
+        if v < 0.1: v_tmrt= 0.1
+        else: v_tmrt= v
+        try:
+            tmrt=((((bg_calib+273.15)**4)+((1.54023939374378*(10**8)*(v_tmrt**0.462186526822537))/(0.95*(0.05**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
+      #     tmrt=((((bg_calib+273.15)**4)+((1.1*(10**8)*(v_tmrt**0.6))/(0.95*(0.04**0.4)))*(bg_calib - dht22_temperature))**0.25)-273.15
+        except (ValueError):
+            tmrt=-9999
 except (IndexError):
     bg_raw = -9999
     bg_calib = -9999
